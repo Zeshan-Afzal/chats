2941fb91ea0cc0fecafb1d302471679a994fd1dd
zeshan-afzal <zeshanafzal259@gmail.com>
2024-08-05 14:20:30 -0700

    Update CORS and Socket.IO configuration

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ import { Server } from 'socket.io'
 import cors from "cors"
 const app =express()
 const server=createServer(app)
-const io=new Server(server, {cors:{origin: "https://chatb-omega.vercel.app/",methods:["GET", "POST"] , credentials: true}} )
 app.use(cors({
   origin: "https://chatb-omega.vercel.app/" // Replace with your client URL
 }));
-  
+
+const io=new Server(server, {cors:{origin: "https://chatb-omega.vercel.app/",methods:["GET", "POST"] , credentials: true}} )
+
 io.on("connection",(socket)=>{
   socket.emit("hello", socket.id)
   console.log(`new user joined with the id ${socket.id}`)
