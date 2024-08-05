b57ca77d96c68d116e1adfcc7afadb7630fcdbc9
zeshan-afzal <zeshanafzal259@gmail.com>
2024-08-05 12:23:24 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import {createServer} from "http"
+import { Server } from 'socket.io'
+const app =express()
+const server=createServer(app)
+const io=new Server(server, {cors:{origin: "http://localhost:5173",methods:["GET", "POST"] , credentials: true}} )
+ 
+  
+io.on("connection",(socket)=>{
+  socket.emit("hello", socket.id)
+  console.log(`new user joined with the id ${socket.id}`)
+  socket.on("send", (message)=>{
+    
+    
+    io.emit("recieve", {message, id:socket.id})
+  })
+  
+})
+
+
+app.get("/",(req, res)=>{
+ res.send("heldfdfow")
+
+})
+
+
+server.listen(3000, ()=>{
+ console.log("app is listi n on threr")
+
+})
