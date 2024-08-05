5435704d3717339afd9231f4b3e1ebc69135de34
zeshan-afzal <zeshanafzal259@gmail.com>
2024-08-05 13:10:38 -0700

    Update CORS settings in index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import {createServer} from "http"
 import { Server } from 'socket.io'
 const app =express()
 const server=createServer(app)
-const io=new Server(server, {cors:{origin: "http://localhost:5173",methods:["GET", "POST"] , credentials: true}} )
+const io=new Server(server, {cors:{origin: "https://chatb-omega.vercel.app",methods:["GET", "POST"] , credentials: true}} )
  
   
 io.on("connection",(socket)=>{
